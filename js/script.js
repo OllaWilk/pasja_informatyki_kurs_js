1781f0ac331b37451abf78f0777cb3bd3b508983
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-03 23:33:44 +0200

    sprawdzenie wartości liczby

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
-/*ZADANIE 1*/
+/*ZADANIE 1 zegar*/
 
 function odliczanie() {
 
-    var dzisiaj = new Date();
+    let dzisiaj = new Date();
 
-    var dzien = dzisiaj.getDate();
-    var miesiac = dzisiaj.getMonth()+1;
-    var rok = dzisiaj.getFullYear();
+    let dzien = dzisiaj.getDate();
+    let miesiac = dzisiaj.getMonth()+1;
+    let rok = dzisiaj.getFullYear();
 
-    var godzina = dzisiaj.getHours();
+    let godzina = dzisiaj.getHours();
     if(godzina<10) godzina = "0" + godzina;
 
-    var minuta = dzisiaj.getMinutes();
+    let minuta = dzisiaj.getMinutes();
     if(minuta<10) minuta = "0" + minuta;
 
-    var sekunda = dzisiaj.getSeconds();
+    let sekunda = dzisiaj.getSeconds();
     if(sekunda<10) sekunda = "0" + sekunda;
 
     document.getElementById("zegar").innerHTML =
     dzien + " / " + miesiac + " / " + rok + " | " + godzina + ":" + minuta + ":" + sekunda;
     setTimeout("odliczanie()", 1000);
 }
+
+/*ZADANIE 2 sprawdzanie liczby*/
+
+function sprawdz () {
+    let liczba = document.getElementById("pole").value;
+
+    if (liczba >0) document.getElementById("wynik").innerHTML="dodatnia";
+    else if (liczba <0 ) document.getElementById("wynik").innerHTML="ujemna";
+    else if (liczba == 0 ) document.getElementById("wynik").innerHTML="zero";
+    else document.getElementById("wynik").innerHTML="to nie jest liczba";
+
+}
+
+/*ZADANIE 3 pokazanie ciągu cyfr zaczynając od pole po lewej do prawej*/
+
